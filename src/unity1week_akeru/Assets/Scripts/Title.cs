e80f58fbb66f557a1e032a2f51a3b7b1c5a05a5a
Parallax_kk <kuronekoya.mail@gmail.com>
2020-12-21 20:56:39 +0900

    Add title

---
+++ src/unity1week_akeru/Assets/Scripts/Title.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using AudioManager;
+
+public class Title : MonoBehaviour
+{
+    /// <summary>
+    /// プレゼントのスプライト
+    /// </summary>
+    [SerializeField]
+    private List<Sprite> m_listPresentSprites = new List<Sprite>();
+
+    /// <summary>
+    /// プレゼント生成
+    /// </summary>
+    [SerializeField]
+    private GameObject m_PresentSpowner = null;
+
+    /// <summary>
+    /// プレゼントのプレハブ
+    /// </summary>
+    [SerializeField]
+    private GameObject m_PresentPrefab = null;
+
+    private void Awake()
+    {
+        BGMManager.Instance.Play(BGMPath.TITLE, 1.0f, 0.0f, 1.0f, true);
+        BGMManager.Instance.ChangeBaseVolume(0.5f);
+        StartCoroutine("CreatePresent");
+    }
+
+    IEnumerator CreatePresent()
+    {
+        while (true)
+        {
+            for (int i = 0; i < Random.Range(0, 10); i++)
+            {
+                GameObject present = Instantiate(m_PresentPrefab, new Vector3(Random.Range(-7.0f, 7.0f), m_PresentSpowner.transform.position.y + Random.Range(0, 7.0f), 0.0f), Quaternion.identity, m_PresentSpowner.transform);
+                present.GetComponent<Rigidbody>().AddTorque(new Vector3(0.0f,0.0f, Random.Range(-20.0f, 20.0f)));
+                present.GetComponent<SpriteRenderer>().sprite = m_listPresentSprites[Random.Range(0, m_listPresentSprites.Count)];
+            }
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
+}
