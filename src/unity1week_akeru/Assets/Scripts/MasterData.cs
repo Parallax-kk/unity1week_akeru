394333e95228c3a420c78a0aa35717d85cd6d8a9
Parallax_kk <kuronekoya.mail@gmail.com>
2020-12-22 23:21:35 +0900

    Add menu scene

---
+++ src/unity1week_akeru/Assets/Scripts/MasterData.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterData : MonoBehaviour
+{
+    /// <summary>
+    /// インスタンス
+    /// </summary>
+    private static MasterData m_Instance = null;
+
+    /// <summary>
+    /// 残り日数
+    /// </summary>
+    private static int m_RemainingDays = 10;
+    public static int GetRemainingDays() { return m_RemainingDays; }
+
+    /// <summary>
+    /// 所持金
+    /// </summary>
+    private static int m_Money = 0;
+    public static void AddMoney(int value) { m_Money += value; }
+    public static int GetMoney() { return m_Money; }
+
+    /// <summary>
+    /// 獲得したヒーロー
+    /// </summary>
+    private static Dictionary<int, bool> m_dicGetHero = new Dictionary<int, bool>();
+    public static void AddHero(int id) { m_dicGetHero[id] = true; }
+
+    private void Awake()
+    {
+        CheckInstance();
+    }
+
+    private bool CheckInstance()
+    {
+        if (m_Instance == null)
+        {
+            m_Instance = (MasterData)this;
+            return true;
+        }
+        else if (m_Instance == this)
+        {
+            return true;
+        }
+
+        Destroy(this);
+        return false;
+    }
+}
---
+++ src/unity1week_akeru/Assets/Scripts/MenuSystem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using TMPro;
+
+public class MenuSystem : MonoBehaviour
+{
+    /// <summary>
+    /// 日数テキスト
+    /// </summary>
+    [SerializeField]
+    private TextMeshProUGUI m_DayText = null;
+
+    /// <summary>
+    /// お金テキスト
+    /// </summary>
+    [SerializeField]
+    private TextMeshProUGUI m_MoneyText = null;
+
+    /// <summary>
+    /// 仕事ボタン押したとき
+    /// </summary>
+    public void ClickWorkBotton()
+    {
+
+    }
+
+    /// <summary>
+    /// ガチャボタン押したとき
+    /// </summary>
+    public void ClickGachaBotton()
+    {
+
+    }
+
+    private void Awake()
+    {
+        m_DayText.text = "残り " + MasterData.GetRemainingDays() + " 日";
+        m_MoneyText.text = MasterData.GetMoney().ToString();
+    }
+}
